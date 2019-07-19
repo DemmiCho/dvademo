ef80793518adb020887f6b2c94dc747aedb34a83
邓艳芝 <dengyanzhi@rainbowcn.com>
2019-07-19 17:20:35 +0800

    save commit

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Button } from 'antd';
 import styles from './IndexPage.css';
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
+      <Button>change0</Button>
+      <Button>change1</Button>
       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
